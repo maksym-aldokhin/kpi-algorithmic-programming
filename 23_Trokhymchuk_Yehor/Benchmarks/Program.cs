dfe336cf6a618d490c28edfd066812c5953f0e3a
20k32 <110043834+20k32@users.noreply.github.com>
2024-03-19 22:05:07 +0200

    Added Lab2 (#4)

---
+++ 23_Trokhymchuk_Yehor/Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Benchmarks;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<Lab2Benchmark>();
+        Console.ReadKey();
+    }
+}
---
+++ 23_Trokhymchuk_Yehor/Lab2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Net.Http.Headers;
+
+namespace Lab2;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        double lowerBound, upperBound, recRes, iterRes, secRecRes;
+
+        Sum sum = new();
+        
+        while (true)
+        {
+            do
+            {
+                Console.Write("Enter lowerBound of arithmetic sum: ");
+            } while (!double.TryParse(Console.ReadLine(), out lowerBound));
+
+            do
+            {
+                Console.Write("Enter upperBound of arithmetic sum: ");
+            } while (!double.TryParse(Console.ReadLine(), out upperBound) || upperBound - lowerBound < 1);
+
+            Console.Clear();
+
+            recRes = sum.RecursiveLog(lowerBound, upperBound);
+            iterRes = sum.IterativeLog(lowerBound, upperBound);
+            secRecRes = sum.SecondVerRecursiveLog(lowerBound, upperBound);
+
+            Console.WriteLine($"Recursive result: {recRes, 30}");
+            Console.WriteLine($"Iterative result: {iterRes, 30}");
+            Console.WriteLine($"Second variant recursive: {secRecRes, 22}\n");
+
+            Console.WriteLine(sum.GetLog());
+
+            Console.WriteLine("\n\nPress ESC to exit.\n");
+
+            if (Console.ReadKey().Key == ConsoleKey.Escape)
+            {
+                break;
+            }
+
+            Console.Clear();
+            sum.ClearLog();
+        }
+    }
+}
